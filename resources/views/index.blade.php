0951cbda3c77b4c5bb57cfba954154489f64838b
Fery <ferym3@gmail.com>
2022-05-19 19:32:47 +0700

    Mengubah file resources/views/index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,8 @@
         <div class="container mt-5">
             <div class="row">
                 <div class="col-lg-8">
-                    <h1>Hallo Teman-teman</h1>
+                    <h1>CRUD LARAVEL</h1>
+                    <a href="{{ url('create') }}" class="btn btn-primary"> Tambah Mahasiswa</a>
                 </div>
             </div>    
         </div>
